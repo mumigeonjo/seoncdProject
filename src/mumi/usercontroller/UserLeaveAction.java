38a369f3790af86cd9da300c3ebbf36846c4e014
minso <minso@192.168.0.147>
2018-05-04 17:42:17 +0900

    민성

--- src/mumi/usercontroller/UserLeaveAction.java
+++ src/mumi/usercontroller/UserLeaveAction.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class UserLeaveAction implements Action{
+public class UserLeaveAction implements Action {
 
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+
+		return mv;
 	}
 
 }
